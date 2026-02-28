4ec88498da09b688626818c664c292ef8d553bf2
V Surin <macbookpro@Vs-MacBook-Pro.local>
2026-02-28 14:39:20 -0800

    some more fun

---
+++ Hack For Humanity/Hack For Humanity/Hack_For_HumanityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hack_For_HumanityApp.swift
+//  Hack For Humanity
+//
+//  Created by V Surin on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Hack_For_HumanityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
